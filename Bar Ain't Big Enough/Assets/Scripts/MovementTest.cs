16d628f5d3021cfd06b6f2535c928366c70dfb90
Niko Bazos <ndb9897@rit.edu>
2017-11-09 01:23:37 -0500

    Added polygon colliders and can now pick up items.

--- Bar Ain't Big Enough/Assets/Scripts/MovementTest.cs
+++ Bar Ain't Big Enough/Assets/Scripts/MovementTest.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementTest : MonoBehaviour {
+public class MovementTest : MonoBehaviour 
+{
 
 	// Use this for initialization
 	void Start () {
@@ -16,20 +17,30 @@ public class MovementTest : MonoBehaviour {
             //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, -0.1f));
             gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, -0.1f);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+		if (Input.GetKey(KeyCode.UpArrow))
         {
             //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, 0.1f)) ;
-            gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, 0.1f);
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, 0.14f);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow))
         {
             gameObject.transform.position = gameObject.transform.position + new Vector3(-0.1f, 0.0f);
             //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(-0.1f, 0.0f));
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+		if (Input.GetKey(KeyCode.RightArrow))
         {
             gameObject.transform.position = gameObject.transform.position + new Vector3(0.1f, 0.0f);
             //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.1f, 0.0f));
         }
     }
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Bottle") 
+		{
+			Destroy (other.rigidbody);
+			other.gameObject.transform.parent = this.transform;
+			other.transform.position = this.transform.position + new Vector3 (0.0f, 0.1f);
+		}
+	}
 }
