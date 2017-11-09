48951e77b1fd22f195f172ba2d1c973d92e72c09
MAIN\wgm8510 <wgm8510@rit.edu>
2017-11-09 09:23:20 -0500

    Added force based movement. Jumping needs major tweak.

--- Bar Ain't Big Enough/Assets/Scripts/MovementTest.cs
+++ Bar Ain't Big Enough/Assets/Scripts/MovementTest.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MovementTest : MonoBehaviour 
 {
-
+	bool hasPickup = false;
+	GameObject heldBottle;
+	public float bottleForce = 0.0f;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,33 +16,51 @@ public class MovementTest : MonoBehaviour
 	void Update () {
 		if (Input.GetKey(KeyCode.DownArrow))
         {
-            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, -0.1f));
-            gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, -0.1f);
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, -75.0f));
+            //gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, -0.1f);
         }
 		if (Input.GetKey(KeyCode.UpArrow))
         {
-            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, 0.1f)) ;
-            gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, 0.14f);
+			//gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 175.0f));
+            gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, 0.1f)) ;
+            //gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, 0.14f);
         }
 		if (Input.GetKey(KeyCode.LeftArrow))
         {
+			//gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-75.0f, 0.0f));
             gameObject.transform.position = gameObject.transform.position + new Vector3(-0.1f, 0.0f);
             //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(-0.1f, 0.0f));
         }
 		if (Input.GetKey(KeyCode.RightArrow))
         {
+			//gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(75.0f, 0.0f));
             gameObject.transform.position = gameObject.transform.position + new Vector3(0.1f, 0.0f);
             //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.1f, 0.0f));
         }
+
+		if (Input.GetMouseButtonDown(0)) {
+			if (hasPickup) {
+				hasPickup = false;
+				heldBottle.transform.parent = null;
+				heldBottle.transform.position = this.transform.position + new Vector3 (2.5f * (Mathf.Cos(this.transform.rotation.z)), 0.2f * (Mathf.Sin(this.transform.rotation.z)), 0.0f);
+				heldBottle.AddComponent<Rigidbody2D> ();
+				heldBottle.GetComponent<Rigidbody2D> ().AddForce (new Vector2(bottleForce, bottleForce));
+			}
+		}
     }
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag == "Bottle") 
 		{
-			Destroy (other.rigidbody);
-			other.gameObject.transform.parent = this.transform;
-			other.transform.position = this.transform.position + new Vector3 (0.0f, 0.1f);
+			if (!hasPickup) {
+				hasPickup = true;
+				Destroy (other.rigidbody);
+				other.gameObject.transform.parent = this.transform;
+				other.transform.position = this.transform.position + new Vector3 (0.0f, 0.1f);
+				heldBottle = other.gameObject;
+			}
+
 		}
 	}
 }
