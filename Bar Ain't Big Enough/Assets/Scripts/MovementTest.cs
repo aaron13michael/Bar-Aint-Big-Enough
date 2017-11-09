6d687e63501772147b23d723c8378b30884b5fe6
hiimhansoo <hxl9400@rit.edu>
2017-11-08 22:39:45 -0500

    Made the level with rigidbodies and stuff. When I went to commit, there was a bunch of scary metadata stuff, so test if this all works first.

---
+++ Bar Ain't Big Enough/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, -0.1f));
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, -0.1f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.0f, 0.1f)) ;
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(-0.1f, 0.0f);
+            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(-0.1f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.1f, 0.0f);
+            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.GetComponent<Rigidbody2D>().position + new Vector2(0.1f, 0.0f));
+        }
+    }
+}
