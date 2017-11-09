aaf5568bd26c19bfbf41356d3ade80b26f357f88
aaron13michael <ams8560@rit.edu>
2017-11-09 07:20:47 -0500

    added player UI with decreasing drunk meter

---
+++ Bar Ain't Big Enough/Assets/Scripts/Sobriety.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sobriety : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector3 startPosition;
+
+    void Start () {
+        startPosition = new Vector3(2.8f, -0.2f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localScale = gameObject.transform.localScale.x > 0 ? 
+            new Vector3(gameObject.transform.localScale.x - 0.1f, gameObject.transform.localScale.y) : new Vector3(60.0f, 4.0f);
+
+        if(gameObject.transform.localScale.x > 0)
+        {
+            gameObject.transform.position += new Vector3(-0.0035f, 0.0f);
+        }
+        else
+        {
+            gameObject.transform.localPosition = startPosition;
+        }
+          
+	}
+}
