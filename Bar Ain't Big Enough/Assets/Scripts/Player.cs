a2f995a10eb40a6a5f7c4ebd56d206c35dd873aa
aaron13michael <ams8560@rit.edu>
2017-11-16 06:08:20 -0500

    health meter spawns in code

--- Bar Ain't Big Enough/Assets/Scripts/Player.cs
+++ Bar Ain't Big Enough/Assets/Scripts/Player.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public int health;
+    int health;
     bool hasPickup = false;
     GameObject heldItem;
     public float itemForce = 0.0f;
 
+    //UI Assets
+    public Sprite healthSprite;
+
     // Use this for initialization
     void Start () {
-		
+        // Generate Health Meter. This assumes the Player UI is the first child of the Player GameObject
+        health = 100;
+
+        Transform healthMeter = gameObject.transform.GetChild(0).GetChild(1);
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject healthChunk = new GameObject("health chunk", typeof(SpriteRenderer));
+            healthChunk.GetComponent<SpriteRenderer>().sprite = healthSprite;
+            healthChunk.transform.localScale = new Vector3(0.6f, 4.0f, 1.0f);
+            healthChunk.transform.SetParent(healthMeter);
+            healthChunk.transform.localPosition = new Vector3(i * 0.042f, 0.0f, 0.0f);
+        }
 	}
 	
 	// Update is called once per frame
@@ -95,6 +109,23 @@ public class Player : MonoBehaviour {
             }
         }
     }
+    /// <summary>
+    /// Applies damage to the player and reflects damage taken through health meter
+    /// </summary>
+    /// <param name="damage">How much damage the player has taken (out of 100)</param>
+    //void applyDamage(int damage)
+    //{
+        //Used to update health meter
+       // int oldHealth = health;
+
+        //Check that health does not go below 0
+        //health = health - damage > 0 ? health - damage : 0;
+        //Transform healthMeter = gameObject.transform.GetChild(0).GetChild(1);
+        //for(int h = oldHealth; h > health; h--)
+        //{
+          //  healthMeter.transform.GetChild(h - 1)
+        //}
+    //}
 
     void Death()
     {
