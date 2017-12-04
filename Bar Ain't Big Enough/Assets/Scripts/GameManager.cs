b704de00858fac44ee4552a7e40c1678bc6174ad
MAIN\ndb9897 <ndb9897@rit.edu>
2017-12-03 22:26:39 -0500

    Added GameManager that finds out the winning player and sends that information through PlayerPrefs which can be used across scenes.

---
+++ Bar Ain't Big Enough/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	// list of players
+	public List<GameObject> players;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// checks to see if there is one player alive
+		if (players.Count <= 1) 
+		{
+			// set the player num as the winning player through PlayerPrefs
+			int pNum = players [0].GetComponent<Player> ().playerNum;
+			PlayerPrefs.SetString (pNum, "Player " + pNum + " Wins!");
+		} 
+		// if there are more than one player alive
+		else 
+		{
+			// loop through the players
+			for (int i = 0; i < players.Count; i++) 
+			{
+				// if a player died remove them from the list
+				if (players [i].GetComponent<Player> ().health <= 0) 
+				{
+					players.RemoveAt (i);
+				}
+			}
+		}
+	}
+}
--- Bar Ain't Big Enough/Assets/Scripts/Player.cs
+++ Bar Ain't Big Enough/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour 
 {
 	// player health
-    int health;
+    public int health;
 
 	bool dead = false;
 
