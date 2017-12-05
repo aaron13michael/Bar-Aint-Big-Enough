9c0bf516798537b347c2af11538ecf516b7ce66c
HanSoo Lee <ssblykos@gmail.com>
2017-12-04 21:23:48 -0500

    Randomly spawns bottles after 2 seconds in 7 specified locations.

---
+++ Bar Ain't Big Enough/Assets/Scripts/SpawnBottles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBottles : MonoBehaviour {
+
+    public GameObject[] spawnLocations;
+    public GameObject bottle;
+
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        SpawnBottle();
+	}
+
+    void SpawnBottle()
+    {
+        if (timer > 2.0f)
+        {
+            int random = Random.Range(0, spawnLocations.Length);
+            Instantiate(bottle, spawnLocations[random].GetComponent<Transform>().position, Quaternion.identity);
+            timer = 0.0f;
+        }
+    }
+}
