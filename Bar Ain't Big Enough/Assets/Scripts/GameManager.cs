3e77a4dc140e38f908cfd33025104caf7bdb3ded
Laptop\Niko <ndb9897@rit.edu>
2017-12-12 16:00:16 -0500

    Had trouble pushing to separate branch, but these are general changes that everyone should have and will not cause errors. Fully functional winner implementation that just needs GUI representation in the added GameOver scene. Also, I made it so that there is a 5 second delay after killing everyone before switching to the GameOver scene.

--- Bar Ain't Big Enough/Assets/Scripts/GameManager.cs
+++ Bar Ain't Big Enough/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour 
 {
@@ -15,27 +16,53 @@ public class GameManager : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		// checks to see if there is one player alive
-		if (players.Count <= 1) 
+		// determining winner in BattleScene
+		if (SceneManager.GetActiveScene ().name == "BattleScene") 
 		{
-			// set the player num as the winning player through PlayerPrefs
-			string pNum = players [0].GetComponent<Player> ().playerNum.ToString();
-			PlayerPrefs.SetString (pNum, "Player " + pNum + " Wins!");
-		} 
-		// if there are more than one player alive
-		else 
-		{
-			/*
-			// loop through the players
-			for (int i = 0; i < players.Count; i++) 
+			// checks to see if there is one player alive
+			if (players.Count <= 1) 
 			{
-				// if a player died remove them from the list
-				if (players [i].GetComponent<Player>().health <= 0) 
+				string pNum = "";
+				// added for loop in order to retrieve correct player number
+				for (int i = 0; i < players.Count; i++) 
 				{
-					players.RemoveAt (i);
+					// set the player num as the winning player through PlayerPrefs
+					pNum = players [i].GetComponent<Player> ().playerNum.ToString ();
+				}
+				PlayerPrefs.SetString ("Winner", "Player " + pNum + " Wins!");
+
+				// delay the LoadGameOver by 5 seconds
+				Invoke ("LoadGameOver", 5.0f);
+			} 
+		// if there are more than one player alive
+			else 
+			{
+				// updates player list 
+				for (int i = 0; i < players.Count; i++) 
+				{
+					// if the player is dead (i.e., null gameobject) remove from the list
+					if (players [i] == null) 
+					{
+						players.RemoveAt (i);
+					}
 				}
 			}
-			*/
 		}
+
+		// Loading the GameOverScene
+		if(SceneManager.GetActiveScene().name == "GameOver")
+		{
+			// retrieve the player pref for winner
+			string winner = PlayerPrefs.GetString("Winner");
+			Debug.Log (winner);
+
+			// modify the text of GUI elment to display the player pref
+		}
+	}
+
+	// separate method to load game over in order to delay the scene load
+	void LoadGameOver()
+	{
+		SceneManager.LoadScene ("GameOver");
 	}
 }
